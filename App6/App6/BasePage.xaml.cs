71e77f6a98debf6bd5f885df170ecaa9103ff962
michalbarczyk <barczykmt@gmail.com>
2020-01-06 17:33:49 +0100

    Add project files.

---
+++ App6/App6/BasePage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class BasePage : ContentPage
+    {
+        public BasePage(ContentView innerView, string pageTitle, ContentView titleBarExtension = null)
+        {
+            // InitializeComponent();
+
+            NavigationPage.SetTitleView(this, new Label { 
+                Text = pageTitle,
+                FontSize = 18,
+                TextColor = Color.Azure,
+                HorizontalTextAlignment = TextAlignment.Center});
+
+            var topButton = new Button { Text = "TOP BUTTON" };
+            var bottomButton = new Button { Text = "BOTTOM BUTTON" };
+
+            StackLayout layout = null;
+
+            if (titleBarExtension == null)
+            {
+                layout = new StackLayout
+                {
+                    Padding = new Thickness(0),
+                    Children =
+                    {
+                        topButton,
+                        innerView,
+                        bottomButton
+                    }
+                };
+            }
+            else
+            {
+                layout = new StackLayout
+                {
+                    Padding = new Thickness(0),
+                    Children =
+                    {
+                        titleBarExtension,
+                        topButton,
+                        innerView,
+                        bottomButton
+                    }
+                };
+            }
+
+            Content = layout;
+        }
+    }
+}
